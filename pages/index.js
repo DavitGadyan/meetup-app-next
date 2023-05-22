188a7316157a5cfd69d8b86ec5aa8c5736926b21
David <davidgadyan92@gmail.com>
2023-05-22 02:07:21 +0200

    vercel

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import { MongoClient } from "mongodb";
+
+import { Fragment } from "react";
+import Head from "next/head";
+import MeetupList from "../components/meetups/MeetupList";
+
+const HomePage = ({ meetups }) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={meetups} />
+    </Fragment>
+  );
+};
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from DB/ API
+  // return object with props key
+  const client = await MongoClient.connect(
+    "mongodb+srv://david:fUBYThLnH127P8OJ@cluster0.xpvi61d.mongodb.net/?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
